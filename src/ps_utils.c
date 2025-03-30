953863a63af64e3c6be07f1e7b2d9157a8b553b3
AlejandroJimenez16 <alejanjiga@gmail.com>
2025-03-30 21:00:59 +0200

    Func free

--- src/ps_utils.c
+++ src/ps_utils.c
@@ -6,18 +6,18 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 22:19:54 by alejandj          #+#    #+#             */
-/*   Updated: 2025/03/26 22:41:32 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/03/30 20:07:19 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	free_stacks(t_stack *stack_a)
+void	free_stack(t_stack *stack)
 {
 	t_list *current;
 	t_list *next;
 	
-	current = stack_a->head;
+	current = stack->head;
 	while (current != NULL)
 	{
 		next = current->next;
@@ -25,5 +25,5 @@ void	free_stacks(t_stack *stack_a)
 		free(current);
 		current = next;
 	}
-	free(stack_a);
+	free(stack);
 }
