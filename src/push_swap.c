86e0af633b7b8c9faaf79153e87bf3515f33f723
AlejandroJimenez16 <alejanjiga@gmail.com>
2025-03-30 21:01:48 +0200

    No normi, validacion ok, leaks casos error

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,21 +6,21 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 12:52:19 by alejandj          #+#    #+#             */
-/*   Updated: 2025/03/26 22:51:31 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/03/30 20:59:39 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	print_errors(char *error, t_stack *stack_a)
+void	print_errors(char *error, t_stack *stack_a, t_stack *stack_b)
 {
 	ft_printf("%s\n", error);
-	if (stack_a)
-		free_stacks(stack_a);
+	free_stack(stack_a);
+	free_stack(stack_b);
 	exit(1);
 }
 
-void	fill_stack(int n, t_stack **stack_a)
+void	fill_stack(int n, t_stack **stack_a, t_stack **stack_b)
 {
 	t_list	*node;
 	t_list *current;
@@ -28,11 +28,11 @@ void	fill_stack(int n, t_stack **stack_a)
 
 	num = malloc(sizeof(int));
 	if (!num)
-		print_errors("Error allocating memory", *stack_a);
+		print_errors("Error allocating memory", *stack_a, *stack_b);
 	*num = n;
 	node = ft_lstnew(num);
 	if (!node)
-		print_errors("Error creating new node", *stack_a);
+		print_errors("Error creating new node", *stack_a, *stack_b);
 	if ((*stack_a)->head == NULL)
 	{
 		(*stack_a)->head = node;
@@ -42,7 +42,7 @@ void	fill_stack(int n, t_stack **stack_a)
 	while (current != NULL)
 	{
 		if (*(int *)(current->content) == n)
-			print_errors("Duplicate numbers", *stack_a);
+			print_errors("Duplicate numbers", *stack_a, *stack_b);
 		current = current->next;
 	}
 	ft_lstadd_back(&((*stack_a)->head), node);
@@ -58,7 +58,7 @@ void	fill_stack(int n, t_stack **stack_a)
 	//---------------------------------------------------
 }
 
-void	validate_fill_stack(int argc, char *argv[], t_stack **stack_a)
+void	validate_fill_stack(int argc, char *argv[], t_stack **stack_a, t_stack **stack_b)
 {
 	int	i;
 	int j;
@@ -80,13 +80,13 @@ void	validate_fill_stack(int argc, char *argv[], t_stack **stack_a)
 				j++;	
 			}
 			if (!ft_isdigit(argv[i][j]))
-				print_errors("Only numbers are allowed", *stack_a);
+				print_errors("Only numbers are allowed", *stack_a, *stack_b);	
 			num = ft_atoi(&argv[i][j]);
 			if (isneg)
 				num = -num;
 			if (num > INT_MAX || num < INT_MIN)
-				print_errors("Number is out of range", *stack_a);
-			fill_stack(num, stack_a);
+				print_errors("Number is out of range", *stack_a, *stack_b);
+			fill_stack(num, stack_a, stack_b);
 			while (argv[i][j] != '\0' && argv[i][j] != ' ')
 				j++;
 		}
@@ -98,23 +98,24 @@ void	init_stacks(t_stack **stack_a, t_stack **stack_b)
 {
 	*stack_a = malloc(sizeof(t_stack));
 	if (!*stack_a)
-		print_errors("Error allocating memory for stack", *stack_a);
+		print_errors("Error allocating memory for stack", *stack_a, *stack_b);
 	(*stack_a)->head = NULL;
 	*stack_b = malloc(sizeof(t_stack));
 	if (!*stack_b)
-		print_errors("Error allocating memory for stack", *stack_b);
+		print_errors("Error allocating memory for stack", *stack_a, *stack_b);
 	(*stack_b)->head = NULL;
 }
 
 int	main(int argc, char *argv[])
 {
-	t_stack *stack_a;
-	t_stack *stack_b;
-	
+	t_stack	*stack_a;
+	t_stack	*stack_b;
+
 	init_stacks(&stack_a, &stack_b);
 	if (argc < 2)
-		print_errors("No arguments", stack_a);
-	validate_fill_stack(argc, argv, &stack_a);
-	free(stack_a);
+		print_errors("No arguments", stack_a, stack_b);
+	validate_fill_stack(argc, argv, &stack_a, &stack_b);
+	free_stack(stack_a);
+	free_stack(stack_b);
 	return (0);
 }
