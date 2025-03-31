da88cb3e3a3601d500ac5cee309b7e4b65d3e8aa
Alejandro Jimenez Garcia <alejandj@c2r9s2.42madrid.com>
2025-03-31 17:14:11 +0200

    Añadiendo func

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,28 +6,29 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 12:23:51 by alejandj          #+#    #+#             */
-/*   Updated: 2025/03/30 20:59:25 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/03/31 14:57:39 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <limits.h>
 # include "../libft/libft.h"
+# include <limits.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 typedef struct s_stack
 {
-    t_list *head;
-} t_stack;
+	t_list	*head;
+}			t_stack;
 
 // Additional Functions
 void	free_stack(t_stack *stack);
+void	print_stack(t_stack *stack);
 
 // Rules
-void    swap_a(t_stack **stack_a);
-void    swap_b(t_stack **stack_b);
+void		swap(t_stack **stack);
+void		push(t_stack **stack_a, t_stack **stack_b);
 
 #endif
