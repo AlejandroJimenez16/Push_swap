eb1d352d14213e516008b863fcc0561f3603e196
Alejandro Jimenez Garcia <alejandj@c3r4s1.42madrid.com>
2025-04-07 17:35:44 +0200

    Reglas añadidas

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 12:23:51 by alejandj          #+#    #+#             */
-/*   Updated: 2025/04/01 16:15:31 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/04/07 17:26:21 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,13 @@ void	free_arr(char **arr);
 void	print_errors(t_stack *stack_a, t_stack *stack_b);
 void	print_stack(t_stack *stack);
 
-
 // Rules
-void		swap(t_stack **stack);
-void		push(t_stack **stack_a, t_stack **stack_b);
+void	swap(t_stack **stack);
+void	ss(t_stack **stack_a, t_stack **stack_b);
+void	push(t_stack **stack_a, t_stack **stack_b);
+void	rotate(t_stack **stack);
+void	rr(t_stack **stack_a, t_stack **stack_b);
+void	reverse_rotate(t_stack **stack);
+void	rrr(t_stack **stack_a, t_stack **stack_b);
 
 #endif
