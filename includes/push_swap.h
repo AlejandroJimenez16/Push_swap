c0a3bb621f3d7b1efded90d998f658474f97db69
Alejandro Jimenez Garcia <alejandj@c2r2s3.42madrid.com>
2025-04-09 17:40:05 +0200

    Reglas separadas añadidas

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 12:23:51 by alejandj          #+#    #+#             */
-/*   Updated: 2025/04/07 17:26:21 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/04/09 17:39:06 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,25 @@ void	print_stack(t_stack *stack);
 
 // Rules
 void	swap(t_stack **stack);
+void	sa(t_stack **stack);
+void	sb(t_stack **stack);
 void	ss(t_stack **stack_a, t_stack **stack_b);
+
 void	push(t_stack **stack_a, t_stack **stack_b);
+void	pa(t_stack **stack_a, t_stack **stack_b);
+void	pb(t_stack **stack_a, t_stack **stack_b);
+
 void	rotate(t_stack **stack);
+void	ra(t_stack **stack_a);
+void	rb(t_stack **stack_b);
 void	rr(t_stack **stack_a, t_stack **stack_b);
+
 void	reverse_rotate(t_stack **stack);
+void	rra(t_stack **stack_a);
+void	rrb(t_stack **stack_b);
 void	rrr(t_stack **stack_a, t_stack **stack_b);
+
+// Sorting functions
+void	sort3(t_stack **stack_a);
 
 #endif
