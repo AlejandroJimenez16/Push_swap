649fd922e537df2e3d1d9a769dc79db9c2c66d2a
AlejandroJimenez16 <alejanjiga@gmail.com>
2025-04-11 14:52:49 +0200

    Funcion sort3 ok y sort4 no funciona

--- src/sort_simple.c
+++ src/sort_simple.c
@@ -6,7 +6,7 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 16:08:50 by alejandj          #+#    #+#             */
-/*   Updated: 2025/04/10 16:18:56 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/04/11 14:47:57 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,42 +30,48 @@ int	is_sort(t_stack **stack)
 	return (1);
 }
 
+void	sort_options(int a, int b, int c, t_stack **stack)
+{
+	if (a > b && b > c)
+		sa(stack);
+	if (a < b && b > a)
+		rra(stack);
+	if (a > b && b < c)
+		sa(stack);
+}
+
 void	sort3(t_stack **stack)
 {
 	t_list	*first;
 	t_list	*last;
-	int		sorted;
+	int		a;
+	int		b;
+	int		c;
 
 	if (!stack || !(*stack) || !(*stack)->head || !(*stack)->head->next)
 		return ;
-	first = (*stack)->head;
-	last = ft_lstlast((*stack)->head);
-	sorted = 0;
-	while (sorted == 0)
+	while (1)
 	{
-		sorted = 1;
-		if (*(int *)(first->content) > *(int *)(first->next->content))
-		{
-			sa(stack);
-			sorted = 0;
-		}
-		if (*(int *)(first->content) > *(int *)(last->content))
-		{
-			rra(stack);
-			sorted = 0;
-		}
-		if (*(int *)(first->next->content) > *(int *)(last->content))
-		{
-			rra(stack);
-			sorted = 0;
-		}
-		sorted = is_sort(stack);
 		first = (*stack)->head;
 		last = ft_lstlast((*stack)->head);
+		a = *(int *)(first->content);
+		b = *(int *)(first->next->content);
+		c = *(int *)(last->content);
+		sort_options(a, b, c, stack);
+		if (is_sort(stack) == 1)
+			break ;
 	}
 }
 
-void	sort4(t_stack **stack_a)
+void	sort4(t_stack **stack_a, t_stack **stack_b)
 {
-	
+	if (!stack_a || !(*stack_a) || !(*stack_a)->head || !(*stack_a)->head->next)
+		return ;
+	if (!stack_b || !(*stack_b) || !(*stack_b)->head || !(*stack_b)->head->next)
+		return ;
+	pb(stack_b, stack_a);
+	print_stack(*stack_a);
+	print_stack(*stack_b);
+	sort3(stack_a);
+	pa(stack_a, stack_b);
 }
