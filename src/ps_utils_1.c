d30007c5e1778fc661cdc700a1de1875fdd5573a
Alejandro Jimenez Garcia <alejandj@c3r6s1.42madrid.com>
2025-04-15 04:31:45 +0200

    Funciones free y print errores

---
+++ src/ps_utils_1.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ps_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/26 22:19:54 by alejandj          #+#    #+#             */
+/*   Updated: 2025/04/15 03:48:38 by alejandj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+void	free_stack(t_stack *stack)
+{
+	t_list	*current;
+	t_list	*next;
+
+	current = stack->head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->content);
+		free(current);
+		current = next;
+	}
+	free(stack);
+}
+
+void	free_arr(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr[i])
+		free(arr[i++]);
+	free(arr);
+}
+
+void	print_errors(t_stack *stack_a, t_stack *stack_b)
+{
+	ft_printf("Error\n");
+	free_stack(stack_a);
+	free_stack(stack_b);
+	exit(1);
+}
+
+void	print_errors_arr(t_stack *stack_a, t_stack *stack_b, char **arr)
+{
+	ft_printf("Error\n");
+	free_stack(stack_a);
+	free_stack(stack_b);
+	free_arr(arr);
+	exit(1);
+}
+
+void	print_stack(t_stack *stack)
+{
+	t_list	*current;
+
+	current = stack->head;
+	while (current != NULL)
+	{
+		ft_printf("%d\n", *(int *)(current->content));
+		current = current->next;
+	}
+}
