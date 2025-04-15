19316b8d341e6baa2957c269f7bc17315692fcae
Alejandro Jimenez Garcia <alejandj@c3r6s1.42madrid.com>
2025-04-15 04:35:10 +0200

    Arreglo leaks (error nums dup)

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 12:52:19 by alejandj          #+#    #+#             */
-/*   Updated: 2025/04/12 20:35:30 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/04/15 04:20:22 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,6 @@ int	is_number(char *arg)
 void	fill_stack(int n, t_stack **stack_a, t_stack **stack_b)
 {
 	t_list	*node;
-	t_list	*current;
 	int		*num;
 
 	num = malloc(sizeof(int));
@@ -46,12 +45,11 @@ void	fill_stack(int n, t_stack **stack_a, t_stack **stack_b)
 		(*stack_a)->head = node;
 		return ;
 	}
-	current = (*stack_a)->head;
-	while (current != NULL)
+	if (is_duplicate(*stack_a, n) == 1)
 	{
-		if (*(int *)(current->content) == n)
-			print_errors(*stack_a, *stack_b);
-		current = current->next;
+		free(node);
+		free(num);
+		print_errors(*stack_a, *stack_b);
 	}
 	ft_lstadd_back(&((*stack_a)->head), node);
 }
@@ -68,14 +66,16 @@ void	validate_fill_stack(int argc, char *argv[], t_stack **stack_a,
 	while (i < argc)
 	{
 		arr = ft_split(argv[i], ' ');
+		if (!arr)
+			print_errors_arr(*stack_a, *stack_b, arr);
 		j = 0;
 		while (arr[j] != NULL)
 		{
 			if (!is_number(arr[j]))
-				print_errors(*stack_a, *stack_b);
+				print_errors_arr(*stack_a, *stack_b, arr);
 			num = ft_atoi(arr[j]);
 			if (num > INT_MAX || num < INT_MIN)
-				print_errors(*stack_a, *stack_b);
+				print_errors_arr(*stack_a, *stack_b, arr);
 			fill_stack(num, stack_a, stack_b);
 			j++;
 		}
@@ -88,7 +88,11 @@ void	init_stacks(t_stack **stack_a, t_stack **stack_b)
 {
 	*stack_a = malloc(sizeof(t_stack));
 	if (!*stack_a)
-		print_errors(*stack_a, *stack_b);
+	{
+		ft_printf("Error\n");
+		free_stack(*stack_a);
+		exit(1);
+	}
 	(*stack_a)->head = NULL;
 	*stack_b = malloc(sizeof(t_stack));
 	if (!*stack_b)
@@ -105,10 +109,10 @@ int	main(int argc, char *argv[])
 	if (argc < 2)
 		print_errors(stack_a, stack_b);
 	validate_fill_stack(argc, argv, &stack_a, &stack_b);
-	print_stack(stack_a);
+	//print_stack(stack_a);
 	manage_sort(&stack_a, &stack_b);
-	ft_printf("Despues\n");
-	print_stack(stack_a);
+	//ft_printf("Despues\n");
+	//print_stack(stack_a);
 	free_stack(stack_a);
 	free_stack(stack_b);
 	return (0);
