b3cbe88e39a2e6881121a3cc080bf35e64a844a7
AlejandroJimenez16 <alejanjiga@gmail.com>
2025-04-21 02:24:57 +0200

    Agregada manage_sort

--- src/ps_utils_2.c
+++ src/ps_utils_2.c
@@ -6,7 +6,7 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 03:59:40 by alejandj          #+#    #+#             */
-/*   Updated: 2025/04/15 04:05:07 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/04/18 23:57:34 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,26 @@ int	is_duplicate(t_stack *stack_a, int n)
 		current = current->next;
 	}
 	return (0);
+}
+
+void	manage_sort(t_stack **stack_a, t_stack **stack_b)
+{
+	int	size;
+	
+	size = ft_lstsize((*stack_a)->head);
+	if (size == 1)
+		return;
+	else if (size == 2)
+	{
+		if (*(int *)((*stack_a)->head->content) > *(int *)((*stack_a)->head->next->content))
+			sa(stack_a);
+	}
+	else if (size == 3)
+		sort3(stack_a);
+	else if (size == 4)
+		sort4(stack_a, stack_b);
+	else if (size == 5)
+		sort5(stack_a, stack_b);
+	else if (size > 5)
+		sort_heavy(stack_a, stack_b);
 }
